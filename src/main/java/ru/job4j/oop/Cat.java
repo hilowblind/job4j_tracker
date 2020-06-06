c0b7f1e6f8e4761c0498df110b6a006be8462cef
Alexey <hilowblind@gmail.com>
2020-06-06 16:12:46 +0400

    1.5. Поля объекта [#293686]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,35 @@
 package ru.job4j.oop;
 
+import java.util.concurrent.Callable;
+
 public class Cat {
+    public String name;
+
+    public String food;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("Cat's name:");
+        System.out.println(this.name);
+        System.out.println("Hi ate:");
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
