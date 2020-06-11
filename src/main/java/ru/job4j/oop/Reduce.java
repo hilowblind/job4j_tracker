0dcba1d1271421a8256332d7386a12da8b3a4028
Alexey <hilowblind@gmail.com>
2020-06-11 04:16:18 +0400

    5.1. Зона видимости переменных [#293693]

--- src/main/java/ru/job4j/oop/Reduce.java
+++ src/main/java/ru/job4j/oop/Reduce.java
@@ -3,8 +3,8 @@ package ru.job4j.oop;
 public class Reduce {
     private int[] array;
 
-    public void to(int[] arrayLocal) {
-        array = arrayLocal;
+    public void to(int[] array) {
+        this.array = array;
     }
 
     public void print() {
